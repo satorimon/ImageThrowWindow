211a34657c4b7cbaf78477057dfdd81dd0202e7b
Satoru.Wada <satoru.wada@optitrack.co.jp>
2020-01-24 09:55:07 +0900

    空のSubWindowが表示される所まで

---
+++ BlankApp1/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using BlankApp1.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System.Windows;
+
+namespace BlankApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            var subwindow = new PrismWindow1();
+            subwindow.Show();
+            RegionManager.SetRegionManager(subwindow, Container.Resolve<IRegionManager>());
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ BlankApp1/Views/PrismWindow1.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace BlankApp1.Views
+{
+    /// <summary>
+    /// Interaction logic for PrismWindow1.xaml
+    /// </summary>
+    public partial class PrismWindow1 : Window
+    {
+        public PrismWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
